8f9da54e2f6f4295941f0214bb6982f421ed029b
Oznobishin_Ruslan <oznobir@gmail.com>
2026-07-02 23:41:09 +0300

    Попытка соединения с блоком с помощью ':'

--- arduino_DigitalPanel.ino
+++ arduino_DigitalPanel.ino
@@ -1,10 +1,21 @@
 #include <SPI.h>
 #include <mcp_can.h>
+//#include <SoftwareSerial.h> // Используем программный порт для инверсии
+
+
 
 const int SPI_CS_PIN = 53;
 MCP_CAN CAN0(SPI_CS_PIN);
 
+// Создаем инвертированный порт: RX на 19 пине, TX на 18 пине
+// параметр (true) заставляет библиотеку инвертировать все сигналы!
+//SoftwareSerial StagSerial(19, 18, true); 
+
 int byteCounter = 0;
+// Массив оригинального запроса параметров для блоков Stag ISA2 ( v11.3 )
+// 0x3A (:) -> Старт, 0x05 -> Длина, 0x66 -> Команда запроса данных, 0x6B -> Чексумма
+byte stagRequest[] = {0x3A, 0x05, 0x66, 0x6B}; 
+
 
 void setup() {
   Serial.begin(115200);  // ПК (в Мониторе порта выберите скорость 115200 и "No line ending / Без конца строки")
@@ -27,17 +38,25 @@ void loop() {
   // Если ввести что-то в Монитор порта на ПК, Ардуино перешлет это в ГБО
   if (Serial.available() > 0) {
     char inputChar = Serial.read();
-    
+    if (inputChar == 's') {
+      while(Serial1.available() > 0) Serial1.read(); // Чистим буфер
+      
+      // Отправляем массив {0x3A, 0x05, 0x66, 0x6B}
+      Serial1.write(stagRequest, sizeof(stagRequest)); 
+      
+      Serial.println(F("\n[ПК -> ГБО] Отправлен пакет запроса 45 байт: 0x3A 0x05 0x66 0x6B"));
+      
+    } else {
     // Очищаем буфер приема перед отправкой, чтобы не перепутать старый мусор с новым ответом
     //while(Serial1.available() > 0) Serial1.read(); 
     
     // Имитируем поведение компьютерного свистка (Сигнал Break)
-    Serial1.end();                 // Временно отключаем UART на пинах 18 и 19
-    pinMode(18, OUTPUT);
-    digitalWrite(18, LOW);         // Жестко прижимаем линию к земле
-    delay(50);                     // Держим 50 мс (Stag проснется от этого падения)
+    //Serial1.end();                 // Временно отключаем UART на пинах 18 и 19
+    //pinMode(18, OUTPUT);
+    //digitalWrite(18, LOW);         // Жестко прижимаем линию к земле
+    //delay(50);                     // Держим 50 мс (Stag проснется от этого падения)
       
-    Serial1.begin(57600);          // Включаем UART обратно
+    //Serial1.begin(57600);          // Включаем UART обратно
     while(Serial1.available() > 0) Serial1.read(); // Чистим эхо
 
     Serial1.write(inputChar); // Пуляем байт в ГБО
@@ -47,6 +66,9 @@ void loop() {
     Serial.print(F("' (HEX: 0x"));
     Serial.print((byte)inputChar, HEX);
     Serial.println(F(")"));
+
+    }
+    
   }
 
   // --- ЧАСТЬ 2: СБОР И АНАЛИЗ ОТВЕТА ОТ ГБО ---
